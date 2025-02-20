c4f17ba7d5a5d1f88eddfa358a07267b222d0401
ikealmighty <isaacyates7@gmail.com>
2025-02-20 16:33:15 -0600

    added Footer component and content container

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router';
-import NavBar from './components/Navbar';
 import RouterConfig from './RouterConfig';
+
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <NavBar />
-    <RouterConfig />
+    <div className="m-2 p-2 max-w-[800px] mx-auto">
+      <NavBar />
+      <RouterConfig />
+      <Footer />
+    </div>
   </BrowserRouter>
 )
