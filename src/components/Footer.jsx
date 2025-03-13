9a3c6b4a52c35124f71bfdd23c3ea83221835e42
ikealmighty <isaacyates7@gmail.com>
2025-03-13 12:43:36 -0500

    added social media links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
+
+  const linkData = [
+    { href: "https://github.com/ikealmighty", title: "Github", icon: faGithub },
+    { href: "https://www.linkedin.com/in/ike-yates-572441116/", title: "LinkedIn", icon: faLinkedin },
+    { href: "https://www.instagram.com/scones.ike.coffee/", title: "IG", icon: faInstagram }
+  ]
+
   return (
-    <div className="w-full bg-jasmine h-[2rem] fixed left-0 bottom-0 p-1 text-center">
-      <span></span>
+    <div className="w-full bg-jasmine h-[2rem] fixed left-0 bottom-0 p-1 flex justify-evenly border-t-2 border-black">
+      {linkData.map((link, index) => (
+        <span key={index}>
+          <NavLink to={link.href} target="_blank">
+            {link.title}
+            <FontAwesomeIcon className="mx-1" icon={link.icon} />
+          </NavLink>
+        </span>
+      ))}
     </div>
   );
 }
